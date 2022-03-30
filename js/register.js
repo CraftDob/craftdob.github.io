aaf4bd275afb5c6566286eb3579536698a4fcbb8
Tejas Mishra <tejas.mishra1234@outlook.com>
2022-03-30 16:32:53 +0530

    Removed Classic Terms and Conditions Into a 21st century thing

--- js/register.js
+++ js/register.js
@@ -1,12 +1,24 @@
 function tos(){
+    
     swal("Terms and Conditions...", "1) Do not share your Username and Password with anyone. ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎2) Do not Place Orders for fun.",);
+
+    document.getElementById('agree').checked="true";
 }
 
 function register(){
-swal({
-    title: "Good job!",
-    text: "You have been Registered Successfully",
-    icon: "success",
-    button: "yay!",
-  })
+
+    swal({
+        title: "Terms and Conditions",
+        text: "1) Do not share your Username and Password with anyone. ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎2) Do not Place Orders for fun.",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true,
+      })
+      .then((willDelete) => {
+        if (willDelete) {
+          window.location.href="/html/home.html";
+        } else {
+          swal("Please Contact our support team. Email- blablah@gmail.com");
+        }
+      });
 };
