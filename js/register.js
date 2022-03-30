8e4dd895437e97808944e73f522b2816fed18369
Tejas Mishra <tejas.mishra1234@outlook.com>
2022-03-30 18:22:52 +0530

    Updates TOS

--- js/register.js
+++ js/register.js
@@ -1,6 +1,6 @@
 function tos(){
     
-    swal("Terms and Conditions...", "1) Do not share your Username and Password with anyone. ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎2) Do not Place Orders for fun.",);
+    swal("Terms and Conditions...", "1) Do not share your Username and Password with anyone. ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ 2) Do not place unnecessary orders. After some strikes, you may be banned",);
 
     document.getElementById('agree').checked="true";
 }
