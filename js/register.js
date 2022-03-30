f1b96e035679b6705edae02c1d0a72e8548064e7
Tejas Mishra <tejas.mishra1234@outlook.com>
2022-03-30 16:48:28 +0530

    bug fixes

--- js/register.js
+++ js/register.js
@@ -6,19 +6,5 @@ function tos(){
 }
 
 function register(){
-
-    swal({
-        title: "Terms and Conditions",
-        text: "1) Do not share your Username and Password with anyone. ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎2) Do not Place Orders for fun.",
-        icon: "warning",
-        buttons: true,
-        dangerMode: true,
-      })
-      .then((willDelete) => {
-        if (willDelete) {
-          window.location.href="/html/home.html";
-        } else {
-          swal("Please Contact our support team. Email- blablah@gmail.com");
-        }
-      });
-};
+    window.location.href="/html/home.html";
+}
