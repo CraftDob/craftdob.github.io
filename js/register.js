ba4cb7e04dec7f792d8fab7e8d1f7e480046b408
Tejas Mishra <tejas.mishra1234@outlook.com>
2022-03-30 16:20:03 +0530

    added terms and conditions

---
+++ js/register.js
@@ -0,0 +1,12 @@
+function tos(){
+    swal("Terms and Conditions...", "1) Do not share your Username and Password with anyone. ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎2) Do not Place Orders for fun.",);
+}
+
+function register(){
+swal({
+    title: "Good job!",
+    text: "You have been Registered Successfully",
+    icon: "success",
+    button: "yay!",
+  })
+};
